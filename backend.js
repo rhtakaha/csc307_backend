16b5c24373ed7c2b055b05f575c618ce6c7b3f49
Ryan <rhtakaha@calpoly.edu>
2022-09-26 13:19:01 -0700

    "through part 2"

--- backend.js
+++ backend.js
@@ -2,6 +2,36 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const users = {
+  users_list: [
+    {
+      id: "xyz789",
+      name: "Charlie",
+      job: "Janitor",
+    },
+    {
+      id: "abc123",
+      name: "Mac",
+      job: "Bouncer",
+    },
+    {
+      id: "ppp222",
+      name: "Mac",
+      job: "Professor",
+    },
+    {
+      id: "yat999",
+      name: "Dee",
+      job: "Aspring actress",
+    },
+    {
+      id: "zap555",
+      name: "Dennis",
+      job: "Bartender",
+    },
+  ],
+};
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -10,4 +40,8 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
+});
+
+app.get("/users", (req, res) => {
+  res.send(users);
 });
